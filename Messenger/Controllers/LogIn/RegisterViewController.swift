ed456d1731579a12abc2fba8489571316d7f8db7
Mohammad <Mohammad_zaini@outlook.com>
2022-12-21 01:51:02 +0300

    Login UI is done

--- Messenger/Controllers/LogIn/RegisterViewController.swift
+++ Messenger/Controllers/LogIn/RegisterViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+      
     }
+    
     
 
     /*
